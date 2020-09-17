b0444328c7464742f66f64dd12eb9e582012da3f
saran <saransurya199@gmail.com>
2020-09-17 22:36:43 +0530

    seond sum facing issue comparing the string array

--- redundent numbers/redundent.c
+++ redundent numbers/redundent.c
@@ -1,13 +1,26 @@
-#include<stdio.h>
-#include<stdlib.h>
-void main(){
-    int *arrval = NULL, size;
-    char numberList;
-    printf("Please type the length of the list : ");
+// C program to store words in an array 
+
+#include <stdio.h> 
+
+void main() 
+{ 
+	int i, j; 
+	int size;
     scanf("%d", &size);
-    arrval = (int *)malloc(size * sizeof(int));
-    if (arrval == NULL){
-        printf("Not enough memory in computer");
+	char array[size][20]; 
+    int count[size];
+    for(i = 0;i<size;i++){
+        count[i] = 0;
     }
-    
-}
+	for (i = 0; i < size; i++){
+		scanf("%s", array[i]); 
+    } 
+	for (i = 0; i < size; i++){
+        for(j = i+1; j<size; j++){
+            array[i] == array[j] ? printf("T\n") : printf("F-----%s\t%s\n",array[j], array[i]);
+        }
+    }
+    for (i = 0; i<size;i++){
+        printf("%d\n", count[i]);
+    }
+} 
