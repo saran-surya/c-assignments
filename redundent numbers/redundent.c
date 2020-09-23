1adef3e65c53fadfe59ecfb38f1eb75faad2ebae
saran <saransurya199@gmail.com>
2020-09-23 10:10:15 +0530

    completed the redundent numbers problem

--- redundent numbers/redundent.c
+++ redundent numbers/redundent.c
@@ -1,26 +1,34 @@
 // C program to store words in an array 
 
 #include <stdio.h> 
-
-void main() 
+#include <string.h>
+#include <stdbool.h>
+int main() 
 { 
-	int i, j; 
-	int size;
-    scanf("%d", &size);
-	char array[size][20]; 
-    int count[size];
-    for(i = 0;i<size;i++){
-        count[i] = 0;
-    }
-	for (i = 0; i < size; i++){
-		scanf("%s", array[i]); 
-    } 
-	for (i = 0; i < size; i++){
-        for(j = i+1; j<size; j++){
-            array[i] == array[j] ? printf("T\n") : printf("F-----%s\t%s\n",array[j], array[i]);
+    int i,j,n;
+    bool flag;
+    printf("Enter the size of array : ");
+    scanf("%d", &n); 
+  
+    // Declaration of 2-D char array 
+    char array[n][20]; 
+  
+    // Initialization of 2-D char array 
+    for (i = 0; i < n; i++) 
+        scanf("%s", array[i]); 
+  
+    // print the words 
+    for(i = 0; i<n;i++){
+        flag = true;
+        for(j = 0; j<n;j++){
+            if(strcmp(array[i], array[j]) == 0 && j != i){
+                flag = false;
+                break;
+            }
+        }
+        if(flag == true){
+            printf("%s\t", array[i]);
         }
     }
-    for (i = 0; i<size;i++){
-        printf("%d\n", count[i]);
-    }
+    return 0; 
 } 
